61601beb52fde9aca6fadba2f9b1fbb5bf720511
Rohit Raj <rij7u7d@gmail.com>
2020-12-30 06:49:36 +0530

    Added className prop to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,64 +1,21 @@
 import React from 'react';
 import { cssColors, cssVariables } from '../../config';
-import { css } from '@emotion/css';
+import { css, cx } from '@emotion/css';
 import lodash from 'lodash';
 import { Spinner } from '../Spinner/Spinner';
 
 export interface IButtonProps {
-    /**
-     * The label for the button
-     * @default "button"
-     */
     label?: string;
-    /**
-     * The state of the button:
-     * * default : default button with label.
-     * * loading : adds a loading spinner (for extra styling, use the `style` property in `loadingSpinner` prop).
-     * * disabled : disables the button, and adds an appropriate cursor on hover.
-     * * disabledLoading : disables the button while also adding a `loadingSpinner`.
-     * @default "default"
-     */
     status?: 'default' | 'loading' | 'disabled' | 'disabledLoading';
-    /**
-     * The HTML specification `type` of the button:
-     * @default "button"
-     */
     type?: 'button' | 'submit' | 'reset';
-    /**
-     * Custom cssProperties styling for the button
-     */
     style?: React.CSSProperties;
-    /**
-     * Configurations for the `loadingSpinner` in case `status` prop is either `loading` or `disabledLoading`
-     */
+    className?: string;
     loadingSpinner?: {
-        /**
-         * Color for the spinner track (static part of the spinner)
-         */
         trackColor: React.CSSProperties['color'];
-        /**
-         * Color for the spinner indicator (moving part of the spinner)
-         */
         indicatorColor: React.CSSProperties['color'];
-        /**
-         * Custom cssProperties styling for the loadingSpinner
-         */
         style?: React.CSSProperties;
     };
-    /**
-     * Controls if the button should be focusable by TabNavigation
-     * * 0 : focusable
-     * * -1 : non-focusable
-     * @default 0
-     */
     tabIndex?: 0 | -1;
-    /**
-     * Callback when the button is clicked
-     * @augments
-     * ```ts
-     * event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-     * ```
-     */
     onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
@@ -105,7 +62,7 @@ export const Button: React.FC<IButtonProps> = (props: IButtonProps): JSX.Element
 
     return (
         <button
-            className={button}
+            className={cx(button, requiredProps.className)}
             onClick={requiredProps.onClick}
             type={requiredProps.type}
             disabled={
