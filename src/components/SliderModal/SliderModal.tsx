22c23043108ca78c0666e4a6d93fe4d128b5e1f5
Rohit Raj <rij7u7d@gmail.com>
2020-12-30 11:22:39 +0530

    Updated SliderModal Component

--- src/components/SliderModal/SliderModal.tsx
+++ src/components/SliderModal/SliderModal.tsx
@@ -1,12 +1,22 @@
 import React, { ReactNode } from 'react';
 import lodash from 'lodash';
-import { css } from '@emotion/css';
+import { css, cx } from '@emotion/css';
 
 export interface ISliderModalProps {
     active: boolean;
     children: ReactNode;
     sliderSize?: '10%' | '20%' | '30%' | '40%' | '50%' | '60%' | '70%' | '80%' | '90%' | '100%'; // on small screeen by default slider width will span to entire width
     onClickBackdrop?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+    style?: {
+        sliderModalWrapperStyle?: React.CSSProperties;
+        backdropOverlayStyle?: React.CSSProperties;
+        sliderContentWrapperStyle?: React.CSSProperties;
+    };
+    className?: {
+        sliderModalWrapper?: string;
+        backdropOverlay?: string;
+        sliderContentWrapper?: string;
+    };
 }
 
 export const SliderModal = (props: ISliderModalProps): JSX.Element => {
@@ -26,13 +36,10 @@ export const SliderModal = (props: ISliderModalProps): JSX.Element => {
         height: 100%;
         z-index: 6;
         overflow: hidden;
-
         transition: ${requiredProps.active
             ? 'background-color 0.2s ease-in-out-out;'
             : 'right 0.3s ease-in-out'};
-
         right: ${requiredProps.active ? 0 : '-100%'};
-
         background: ${requiredProps.active ? 'rgb(0, 0, 0, 0.4)' : 'transparent'};
     `;
 
@@ -52,15 +59,25 @@ export const SliderModal = (props: ISliderModalProps): JSX.Element => {
         top: 0;
         overflow-y: auto;
         transition: right 0.2s ease-in-out;
-
         width: ${requiredProps.sliderSize};
         right: ${requiredProps.active ? 0 : '-100%'};
     `;
 
     return (
-        <div className={sliderModalWrapper}>
-            <div className={backdropOverlay} />
-            <div className={sliderContentWrapper}>{requiredProps.children}</div>
+        <div
+            className={cx(sliderModalWrapper, requiredProps.className?.sliderModalWrapper)}
+            style={requiredProps.style?.sliderModalWrapperStyle}
+        >
+            <div
+                className={cx(backdropOverlay, requiredProps.className?.backdropOverlay)}
+                style={requiredProps.style?.backdropOverlayStyle}
+            />
+            <div
+                className={cx(sliderContentWrapper, requiredProps.className?.sliderContentWrapper)}
+                style={requiredProps.style?.sliderContentWrapperStyle}
+            >
+                {requiredProps.children}
+            </div>
         </div>
     );
 };
