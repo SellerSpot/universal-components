063fd9fef54e52d98ab4895265e8fe2f64ba3aa5
Rohit Raj <rij7u7d@gmail.com>
2020-12-30 12:08:36 +0530

    Updated Dropdown component

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,21 +1,29 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { css } from '@emotion/css';
+import { css, cx } from '@emotion/css';
 import lodash from 'lodash';
 import { FaCaretDown } from 'react-icons/fa';
 import { cssColors, cssVariables } from '../../config';
+import { getDropdownClasses, IGetDropdownClasses } from './dropdown.styles';
 
 export interface IDropdownProps {
-    options?: string[];
+    options?: string[] | JSX.Element[];
     label?: string;
     helperText?: string;
     error?: {
         showError: boolean;
         errorMessage: string;
     };
-    onSelect: (option: string) => void;
-    style?: React.CSSProperties;
-    labelStyle?: React.CSSProperties;
-    helperStyle?: React.CSSProperties;
+    onSelect: (option: number) => void;
+    style?: {
+        labelStyle?: React.CSSProperties;
+        dropDownBoxStyle?: React.CSSProperties;
+        dropDownSelectStyle?: React.CSSProperties;
+        caretIconStyle?: React.CSSProperties;
+        dropDownListStyle?: React.CSSProperties;
+        dropDownItemStyle?: React.CSSProperties;
+        helperTextStyle?: React.CSSProperties;
+    };
+    className?: IGetDropdownClasses;
 }
 
 export const Dropdown: React.FC<IDropdownProps> = (props: IDropdownProps): JSX.Element => {
@@ -32,6 +40,7 @@ export const Dropdown: React.FC<IDropdownProps> = (props: IDropdownProps): JSX.E
     };
 
     const requiredProps = lodash.merge(defaultProps, props);
+    const classNames = getDropdownClasses(requiredProps, showOptions);
 
     const handleClickOutsideHandler = useCallback(() => shouldShowOptions(!showOptions), [
         shouldShowOptions,
@@ -46,119 +55,69 @@ export const Dropdown: React.FC<IDropdownProps> = (props: IDropdownProps): JSX.E
         };
     }, [showOptions, handleClickOutsideHandler]);
 
-    const label = css`
-        display: block;
-        font-size: ${cssVariables['--font-size-secondary']};
-        font-weight: 600;
-        margin-bottom: 5px;
-    `;
-
-    const dropDownBox = css`
-        display: block;
-        height: ${cssVariables['--input-field-height']};
-        width: 100%;
-        border: 1px solid ${cssColors['--input-border-color']};
-        border-radius: ${cssVariables['--border-radius']};
-        transition: all ${cssVariables['--transition-duration']};
-        cursor: pointer;
-        font-size: ${cssVariables['--font-size-secondary']};
-        font-weight: 400;
-        position: relative;
-
-        :hover {
-            border-color: ${cssColors['--input-focus-border-color']};
-        }
-    `;
-
-    const dropDownSelect = css`
-        width: 100%;
-        height: 100%;
-        padding: 0 10px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    `;
-
-    const caretIcon = css`
-        color: ${cssColors['--primary-font-color']};
-    `;
-
-    const dropDownList = css`
-        width: 100%;
-        max-height: 140px;
-        height: auto;
-        position: absolute;
-        top: ${cssVariables['--input-field-height']};
-        border: 1px solid ${cssColors['--input-border-color']};
-        border-radius: ${cssVariables['--border-radius']};
-        user-select: none;
-        z-index: ${cssVariables['--z-index-dropdown']};
-        font-size: ${cssVariables['--font-size-secondary']};
-        font-weight: 400;
-        overflow-y: auto;
-        background-color: ${cssColors['--primary-background-color']};
-        box-shadow: 0 0 5px 0px ${cssColors['--overlay-color']};
-        transition: ${cssVariables['--transition-duration']};
-
-        opacity: ${showOptions ? 1 : 0};
-        visibility: ${showOptions ? 'visible' : 'hidden'};
-    `;
-
-    const dropDownItem = css`
-        width: 100%;
-        padding: 6px 10px;
-        display: flex;
-        align-items: center;
-        justify-content: flex-start;
-        background-color: ${cssColors['--primary-background-color']};
-    `;
-
-    const helperText = css`
-        font-size: ${cssVariables['--font-size-tertiary']};
-        margin-top: 2px;
-        font-weight: 400;
-        color: ${requiredProps.error?.showError
-            ? cssColors['--danger-color']
-            : cssColors['--tertiary-font-color']};
-    `;
-
     return (
         <div>
             {requiredProps.label ?? false ? (
-                <label className={label} style={requiredProps.labelStyle}>
+                <label
+                    className={cx(classNames.label, requiredProps.className?.label)}
+                    style={requiredProps.style?.labelStyle}
+                >
                     {requiredProps.label}
                 </label>
             ) : null}
-            <div className={dropDownBox}>
+            <div
+                className={cx(classNames.dropDownBox, requiredProps.className?.dropDownBox)}
+                style={requiredProps.style?.dropDownBoxStyle}
+            >
                 <div
-                    className={dropDownSelect}
+                    className={cx(
+                        classNames.dropDownSelect,
+                        requiredProps.className?.dropDownSelect,
+                    )}
+                    style={requiredProps.style?.dropDownSelectStyle}
                     tabIndex={0}
                     onClick={() => shouldShowOptions(!showOptions)}
                 >
-                    <p>{requiredProps.options[selectedOption]}</p>
-                    <FaCaretDown className={caretIcon} />
+                    {requiredProps.options[selectedOption]}
+                    <FaCaretDown
+                        className={cx(classNames.caretIcon, requiredProps.className?.caretIcon)}
+                        style={requiredProps.style?.caretIconStyle}
+                    />
                 </div>
-                <div className={dropDownList}></div>
-                <ul>
-                    {requiredProps.options.map((option, index) => {
-                        return (
-                            <li
-                                onClick={() => {
-                                    setSelectedOption(index);
-                                    shouldShowOptions(false);
-                                    requiredProps.onSelect(option);
-                                }}
-                                key={index}
-                                className={dropDownItem}
-                            >
-                                {option}
-                            </li>
-                        );
-                    })}
-                </ul>
+                <div
+                    className={cx(classNames.dropDownList, requiredProps.className?.dropDownList)}
+                    style={requiredProps.style?.dropDownListStyle}
+                >
+                    <ul>
+                        {(requiredProps.options as Array<string | JSX.Element>).map(
+                            (option, index) => {
+                                return (
+                                    <li
+                                        onClick={() => {
+                                            setSelectedOption(index);
+                                            shouldShowOptions(false);
+                                            requiredProps.onSelect(index);
+                                        }}
+                                        key={index}
+                                        className={cx(
+                                            classNames.dropDownItem,
+                                            requiredProps.className?.dropDownItem,
+                                        )}
+                                        style={requiredProps.style?.dropDownItemStyle}
+                                    >
+                                        {option}
+                                    </li>
+                                );
+                            },
+                        )}
+                    </ul>
+                </div>
             </div>
             {requiredProps.helperText !== undefined || requiredProps.error !== undefined ? (
-                <label className={helperText} style={requiredProps.helperStyle}>
+                <label
+                    className={cx(classNames.helperText, requiredProps.className?.helperText)}
+                    style={requiredProps.style?.helperTextStyle}
+                >
                     {requiredProps.error?.showError
                         ? requiredProps.error.errorMessage
                         : requiredProps.helperText}
