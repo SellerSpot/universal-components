bca4a55846d0c475523fd12a17c6134e98272b79
Rohit Raj <rij7u7d@gmail.com>
2021-02-06 13:11:38 +0530

    Removed react-hotkeys-hook

--- src/components/SliderModal/SliderModal.tsx
+++ src/components/SliderModal/SliderModal.tsx
@@ -1,6 +1,5 @@
 import lodash from 'lodash';
-import React from 'react';
-import { useHotkeys } from 'react-hotkeys-hook';
+import React, { useEffect } from 'react';
 import { MdClose } from 'react-icons/md';
 import { cx } from '@emotion/css';
 import { getSliderModalStyles } from './sliderModal.styles';
@@ -17,17 +16,24 @@ export const SliderModal = (props: ISliderModalProps): JSX.Element => {
 
     const requiredProps = lodash.merge(defaultProps, props);
     const styles = getSliderModalStyles(requiredProps);
-    useHotkeys(
-        'esc',
-        (event) => {
-            if (requiredProps.active) {
-                requiredProps.onClickEsc(event);
-            }
-        },
-        {
-            enableOnTags: ['INPUT', 'SELECT', 'TEXTAREA'],
-        },
-    );
+
+    // handler for the keydown listener
+    const handleKeydown = (event: KeyboardEvent) => {
+        if (event.key === 'Escape') {
+            requiredProps.onClickEsc(event);
+        }
+    };
+
+    //# Keydown event listeners
+    useEffect(() => {
+        // attaching keydown listener when the slider is active
+        if (requiredProps.active) {
+            document.addEventListener('keydown', handleKeydown);
+        }
+        return () => {
+            document.removeEventListener('keydown', handleKeydown);
+        };
+    }, [requiredProps.active]);
 
     return (
         <div
