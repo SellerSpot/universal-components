2bb8faef32dac87f6bbf04910f78d175096dbb19
Rohit Raj <rij7u7d@gmail.com>
2021-02-08 17:32:33 +0530

    Added positioning for the sliderModal

--- src/components/SliderModal/sliderModal.types.ts
+++ src/components/SliderModal/sliderModal.types.ts
@@ -26,10 +26,7 @@ export interface ISliderModalProps {
     children: JSX.Element;
     sliderSize?: '10%' | '20%' | '30%' | '40%' | '50%' | '60%' | '70%' | '80%' | '90%' | '100%'; // on small screeen by default slider width will span to entire width
     zIndex?: number;
-<<<<<<< HEAD
     type?: 'fixed' | 'absolute';
-=======
->>>>>>> 63dc32c0825b7beb0797a749f8e5249a079dffac
     onClickBackdrop?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
     onClickEsc?: (event: KeyboardEvent) => void;
     style?: {
