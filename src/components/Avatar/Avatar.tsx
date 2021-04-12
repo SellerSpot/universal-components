8723d718425722829406c1a7afdb1d63021aa55c
Rohit Raj <rij7u7d@gmail.com>
2021-04-12 12:21:47 +0530

    components: Completed Avatar Component

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,36 @@
+import cn from 'classnames';
+import React, { ReactElement } from 'react';
+import styles from './Avatar.module.scss';
+import { IAvatarProps } from './Avatar.types';
+export { IAvatarProps } from './Avatar.types';
+
+export const Avatar = (props: IAvatarProps): ReactElement => {
+    const { content, disabled, events, size, theme, variant } = props;
+
+    const className = !disabled
+        ? cn(
+              styles.wrapper,
+              { [styles.selectedNoBg]: theme === 'selectedNoBg' },
+              { [styles.selected]: theme === 'selected' },
+              { [styles.unselected]: theme === 'unselected' },
+              { [styles.smallWrapper]: size === 'small' },
+              { [styles.circular]: variant === 'circular' },
+              { [styles.rounded]: variant === 'rounded' },
+              { [styles.square]: variant === 'square' },
+          )
+        : cn(styles.wrapper, styles.disabled, {
+              [styles.smallWrapper]: size === 'small',
+          });
+
+    return (
+        <div
+            className={className}
+            onClick={events?.onClick}
+            onFocus={events?.onFocus}
+            onMouseLeave={events?.onMouseLeave}
+            onMouseOver={events?.oneMouseOver}
+        >
+            {content}
+        </div>
+    );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './Alert/Alert';
+export * from './AppPreloader/AppPreloader';
+export * from './Avatar/Avatar';
 export * from './ThemeProvider/ThemeProvider';
