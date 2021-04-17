8977e386945b38bdb69fa303c2d7dfad79be715c
Rohit Raj <rij7u7d@gmail.com>
2021-04-17 05:33:14 +0530

    Switch component implemented

---
+++ src/components/Switch/Switch.types.ts
@@ -0,0 +1,19 @@
+import { IColors, IFontSizes, TMuiThemes } from '../../theme';
+
+export interface ISwitchProps {
+    /**
+     * Toggles the selected state
+     */
+    checked?: boolean;
+    /**
+     * function(event: object) => void
+     * @props event: The event source of the callback. You can pull out the new value by accessing
+     * @accessing event.target.value (string).
+     * event.target.checked (boolean).
+     */
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    theme?: keyof TMuiThemes;
+    size?: 'small' | 'medium';
+    fontSizes: IFontSizes;
+    colors: IColors;
+}
