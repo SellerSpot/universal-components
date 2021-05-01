66493e43ab3416b341b82903ecd247792e95625f
Rohit Raj <rij7u7d@gmail.com>
2021-05-01 13:14:33 +0530

    Removed sortable tree from universal components

--- src/components/SortableTree/SortableTree.types.ts
+++
@@ -1,6 +0,0 @@
-// import { TreeItem } from 'react-sortable-tree';
-
-// export interface ISortableTreeProps {
-//     treeData: TreeItem[];
-//     onChangeTreeData?: (treeData: TreeItem[]) => void;
-// }
