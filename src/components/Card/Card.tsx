37ff6a34bb3426b87462c6ee56b44b154273a386
Rohit Raj <rij7u7d@gmail.com>
2021-05-26 10:40:08 +0530

    Improved slider and Card Components

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,18 +1,21 @@
 import React, { ReactElement } from 'react';
 import { Card as MUICard, CardActions, CardContent, CardMedia } from '@material-ui/core';
+import styles from './Card.module.scss';
+import cn from 'classnames';
 import { ICardProps } from './Card.types';
 
 export { ICardProps } from './Card.types';
 
 export const Card = (props: ICardProps): ReactElement => {
-    const { actions, className, content, media, onClick } = props;
+    const { actions, className, content, image, onClick } = props;
+    const cardMediaClassName = cn(styles.mediaDefault, className?.mediaWrapper);
     return (
         <MUICard onClick={onClick} className={className?.cardWrapper}>
-            {!!media ? <CardMedia className={className?.mediaWrapper}>{media}</CardMedia> : null}
-            {!!content ? (
+            {image ? <CardMedia className={cardMediaClassName} image={image} /> : null}
+            {content ? (
                 <CardContent className={className?.contentWrapper}>{content}</CardContent>
             ) : null}
-            {!!actions ? (
+            {actions ? (
                 <CardActions className={className?.actionsWrapper}>{actions}</CardActions>
             ) : null}
         </MUICard>
