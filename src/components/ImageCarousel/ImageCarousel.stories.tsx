ab4e03a0bfd927ef16fb4007549ae7418e6b80d3
ThayalanGR <grthayalan18@gmail.com>
2021-05-30 02:07:38 +0530

    craousel component enhancements done

--- src/components/ImageCarousel/ImageCarousel.stories.tsx
+++ src/components/ImageCarousel/ImageCarousel.stories.tsx
@@ -27,6 +27,18 @@ ImageCarousel.args = {
             imageUrl:
                 'https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
         },
+        {
+            imageUrl:
+                'https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        },
+        {
+            imageUrl:
+                'https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        },
+        {
+            imageUrl:
+                'https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        },
     ],
     height: '300px',
 } as IImageCarouselProps;
--- src/components/ImageCarousel/ImageCarousel.tsx
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -1,7 +1,12 @@
-import React, { ReactElement } from 'react';
+import React, { CSSProperties, ReactElement, useRef } from 'react';
 import AliceCarousel from 'react-alice-carousel';
+import cn from 'classnames';
+
+import { IconButton } from '../IconButton/IconButton';
 import { Image } from '../Image/Image';
 import { IImageCarouselProps } from './ImageCarousel.types';
+import styles from './ImageCarousel.module.scss';
+import { ICONS } from '../../utilities/icons';
 
 export { IImageCarouselProps } from './ImageCarousel.types';
 
@@ -14,28 +19,61 @@ const responsive = {
 };
 
 export const ImageCarousel = (props: IImageCarouselProps): ReactElement => {
+    // props
     const { images, height } = props;
 
+    // helpers
+    const carouselRef = useRef<AliceCarousel>();
+
+    // handlers
+    const onClickHandler = (direction: 'forward' | 'backward') => () => {
+        if (direction === 'forward') {
+            carouselRef?.current?.slideNext(null);
+        } else {
+            carouselRef?.current?.slidePrev(null);
+        }
+    };
+
+    // draw
+    const divStyle: CSSProperties = {
+        height,
+    };
     return (
-        <AliceCarousel
-            mouseTracking
-            controlsStrategy="responsive"
-            keyboardNavigation
-            disableButtonsControls={true}
-            touchTracking
-            responsive={responsive}
-            items={images.map((image, imageIndex) => {
-                const { imageUrl } = image;
-                return (
-                    <div
-                        key={imageIndex}
-                        style={{ height, padding: '0 5px' }}
-                        onDragStart={handleDragStart}
-                    >
-                        <Image src={imageUrl} />
-                    </div>
-                );
-            })}
-        />
+        <div className={styles.imageCarouselWrapper} style={divStyle}>
+            <AliceCarousel
+                mouseTracking
+                controlsStrategy="responsive"
+                keyboardNavigation
+                disableButtonsControls={true}
+                touchTracking
+                responsive={responsive}
+                infinite={true}
+                ref={carouselRef}
+                items={images.map((image, imageIndex) => {
+                    const { imageUrl } = image;
+                    return (
+                        <div
+                            key={imageIndex}
+                            style={{ height, padding: '0 5px' }}
+                            onDragStart={handleDragStart}
+                        >
+                            <Image src={imageUrl} />
+                        </div>
+                    );
+                })}
+            />
+            <IconButton
+                size="medium"
+                className={cn(styles.actionButton, styles.actionButtonLeft)}
+                icon={<ICONS.MdKeyboardArrowLeft className={styles.actionButtonIcon} />}
+                onClick={onClickHandler('backward')}
+            />
+            <IconButton
+                size="medium"
+                className={cn(styles.actionButton, styles.actionButtonRight)}
+                icon={<ICONS.MdKeyboardArrowRight className={styles.actionButtonIcon} />}
+                onClick={onClickHandler('forward')}
+            />
+        </div>
     );
 };
